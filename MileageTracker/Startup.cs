d7fce3d1011fe2fadd88e461852533182fddd6d6
nvdvlies <niels.vandervlies@gmail.com>
2014-09-29 20:59:58 +0200

    Access-Control-Allow-Origin

--- MileageTracker/Startup.cs
+++ MileageTracker/Startup.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Owin;
+﻿using System.Web.Http.Cors;
+using Microsoft.Owin;
 using Microsoft.Owin.Cors;
 using Microsoft.Owin.Security.OAuth;
 using MileageTracker.Infrastructure.Configuration;
@@ -13,7 +14,7 @@ namespace MileageTracker {
     public class Startup {
         public void Configuration(IAppBuilder app) {
             var config = new HttpConfiguration();
-            config.EnableCors();
+            config.EnableCors(new EnableCorsAttribute("*", "*", "*"));
 
             ApplicationSettingsFactory.InitializeApplicationSettingsFactory(new WebConfigApplicationSettings());
             LoggingFactory.InitializeLogFactory(new Log4NetAdapter());
@@ -44,6 +45,25 @@ namespace MileageTracker {
 
             app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
             app.UseOAuthAuthorizationServer(oAuthServerOptions);
+
+            app.Use(async (context, next) =>
+            {
+                var request = context.Request;
+                var response = context.Response;
+                if (request.Path.StartsWithSegments(oAuthServerOptions.TokenEndpointPath)) {
+                    var origin = request.Headers.Get("Origin");
+                    if (!string.IsNullOrEmpty(origin)) {
+                        response.Headers.Set("Access-Control-Allow-Origin", origin);
+                    }
+                    if (request.Method == "OPTIONS") {
+                        response.StatusCode = 200;
+                        response.Headers.AppendCommaSeparatedValues("Access-Control-Allow-Methods", "GET", "POST");
+                        response.Headers.AppendCommaSeparatedValues("Access-Control-Allow-Headers", "authorization", "content-type");
+                        return;
+                    }
+                }
+                await next();
+            });
         }
     }
 }
